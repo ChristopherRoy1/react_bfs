d54b515abf22aa33a03989e4669a8cd41c39a376
Christopher Roy <christopher.roy@mail.mcgill.ca>
2020-03-22 15:24:48 -0400

    added hex corner function

--- src/BFS/BFS.jsx
+++ src/BFS/BFS.jsx
@@ -8,10 +8,34 @@ export default class BFS extends React.Component {
   }
 
 
+  componentWillMount(){
+      this.setState({
+        canvasSize: {canvasWidth: 800, canvasHeight: 600}
+      })
+  }
+
+  componentDidMount() {
+    const { canvasWidth, canvasHeight } = this.state.canvasSize;
+
+    this.canvasHex.width = canvasWidth;
+    this.canvasHex.height = canvasHeight;
+    this.drawHex(this.canvasHex, { x: 50, y: 50});
+  }
+
+  getHexCornerCoord(center, i){}
+    let angle_deg = 60 * i - 30;
+    let angle_rad = Math.PI / 180 * angle_deg;
+
+    let x = center.x + this.state.hexSize * cos(angle_rad);
+    let y = center.y + this.state.hexSize * sin(angle_rad);
+    return this.Point(x, y);
+
+      }
+
   render() {
     return (
       <div className="BFS">
-        <canvas></canvas>
+        <canvas ref={ canvasHex => this.canvasHex = canvasHex}></canvas>
       </div>
     )
   }
